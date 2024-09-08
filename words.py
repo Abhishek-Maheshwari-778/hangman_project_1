fb886f55b3e27c19ec5cb05cbb28d5591845c6cf
Abhishek Maheshwari <156508011+Abhishek-Maheshwari-778@users.noreply.github.com>
2024-09-08 23:52:36 +0530

    Add files via upload

---
+++ words.py
@@ -0,0 +1,13 @@
+word_list = [
+    "able", "about", "acid", "across", "act", "air", "all", "amount", "and", "animal",
+    "apple", "army", "art", "baby", "back", "bad", "bag", "ball", "band", "base",
+    "bath", "be", "bed", "bee", "bell", "bird", "bite", "black", "blade", "blood",
+    "blue", "boat", "body", "bone", "book", "boy", "brain", "bread", "bridge", "brother",
+    "brown", "brush", "burn", "business", "butter", "button", "cake", "cat", "cause", "chain",
+    "chalk", "cheese", "circle", "clean", "clock", "cloud", "coal", "coat", "cold", "comb",
+    "corn", "cow", "cup", "day", "dog", "door", "dream", "drink", "duck", "dust",
+    "ear", "egg", "eye", "face", "farm", "fear", "feet", "fire", "fish", "flag",
+    "floor", "flower", "fly", "food", "foot", "fork", "fox", "fruit", "game", "garden",
+    "girl", "glass", "goat", "gold", "grass", "green", "hand", "hat", "head", "heart",
+    "hill", "horse", "house", "key", "king", "knee", "knife", "lamp", "leg", "letter"
+]
